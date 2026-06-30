81f7d2f8e9320cf51ab846bdf1a5f0c0bbb2f351
nilou <ytvannilou0192@gmail.com>
2026-06-30 18:30:42 +0800

    fix: fixed folder structure to include stages

--- src/app/(app)/projects/[projectId]/phases/[phaseId]/modules/[moduleId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ModulePage({
-  params,
-}: {
-  params: { projectId: string; phaseId: string; moduleId: string };
-}) {
-  return <div>Module {params.moduleId}</div>;
-}
--- src/app/(app)/projects/[projectId]/phases/[phaseId]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PhasePage({
-  params,
-}: {
-  params: { projectId: string; phaseId: string };
-}) {
-  return <div>Phase {params.phaseId}</div>;
-}
